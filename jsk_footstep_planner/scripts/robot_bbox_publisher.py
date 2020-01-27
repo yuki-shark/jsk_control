cfb80455da5b11d29dc7d7adfb3c325d0dfedbc6
Yuki Omori <ohmori@jsk.imi.i.u-tokyo.ac.jp>
2020-01-27 22:41:17 +0900

    clip cart pointcloud by robot_bbox

---
+++ jsk_footstep_planner/scripts/robot_bbox_publisher.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+from std_srvs.srv import Empty as EmptySrv
+from jsk_recognition_msgs.msg import BoundingBox
+from jsk_footstep_msgs.msg import FootstepArray
+from geometry_msgs.msg import Point, Quaternion, Vector3
+
+rospy.init_node("robot_bbox_publisher", anonymous=True)
+p = rospy.Publisher("/robot_bbox", BoundingBox, queue_size=10)
+r = rospy.Rate(100)
+
+rospy.loginfo("Clearing /accumulated_heightmap/reset due to change of robot_bbox")
+srv = rospy.ServiceProxy("/accumulated_heightmap/reset", EmptySrv)
+try:
+  srv()
+except:
+  rospy.logerr("Failed to reset /accumulated_heightmap")
+
+bbox_msg = BoundingBox()
+bbox_msg.header.frame_id = 'body_on_odom'
+position = Point(0.75, 0.0, 1.25)
+orientation = Quaternion(0.0, 0.0, 0.0, 0.0)
+dimensions = Vector3(1.5, 1.2, 3.0)
+bbox_msg.pose.position = position
+bbox_msg.pose.orientation = orientation
+bbox_msg.dimensions = dimensions
+
+while not rospy.is_shutdown():
+  p.publish(bbox_msg)
+  r.sleep()
